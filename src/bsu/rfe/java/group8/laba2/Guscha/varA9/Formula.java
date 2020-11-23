395028e1456157c881beb96115333aafd9da283d
unknown <usermaksim.gushha@mail.ru>
2020-11-23 12:00:18 +0300

    Сделал

---
+++ src/bsu/rfe/java/group8/laba2/Guscha/varA9/Formula.java
@@ -0,0 +1,139 @@
+package bsu.rfe.java.group8.laba2.Guscha.varA9;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Formula extends JFrame{
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 400;
+    //поля для считывания х, у, z и результата
+    private JTextField textFieldX;
+    private JTextField textFieldY;
+    private JTextField textFieldZ;
+    private JTextField textFieldResult;
+    //радио кнопки
+    private ButtonGroup radioButtons = new ButtonGroup();
+    //Контейнер для отображения радио кнопок
+    private Box hboxFormulaType = Box.createHorizontalBox();
+    //идентификатор выбранной формулы
+    private int formulaId = 1;
+    //Формула 1 для расчета
+    public Double Formula_1 (double x, double y, double z){
+        return Math.sin(Math.sin(y) + Math.pow(2.718, Math.cos(y)) + z * z) * Math.pow(Math.sin(3.14159 * y * y) + Math.log(x * x), 1/4);
+    }
+    //Формула 2 для расчета
+    public Double Formula_2 (double x, double y, double z){
+        return (Math.atan(Math.pow(z, x))) / (y * y + z * Math.sin(Math.log(x)));
+    }
+    //метод для добавления радио кнопок
+    private void addRadioButton(String buttonName, final int formulaId){
+        JRadioButton button = new JRadioButton(buttonName);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                Formula.this.formulaId = formulaId;
+            }
+        });
+        radioButtons.add(button);
+        hboxFormulaType.add(button);
+    }
+    public Formula(){
+        //заголовок
+        super("Калькулятор формулы");
+        //установка размера и положения окна
+        setSize(WIDTH,HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH) / 2,(kit.getScreenSize().height - HEIGHT) / 2);
+        //Добавление радио кнопок
+        hboxFormulaType.add(Box.createHorizontalGlue());
+        addRadioButton("Формула 1", 1);
+        addRadioButton("Формула 2", 2);
+        radioButtons.setSelected(radioButtons.getElements().nextElement().getModel(),true);
+        hboxFormulaType.add(Box.createHorizontalGlue());
+        //Добавление текстовых полей для переменных
+        JLabel labelX = new JLabel("X:");
+        textFieldX = new JTextField("0", 10);
+        textFieldX.setMaximumSize(textFieldX.getPreferredSize());
+        JLabel labelY = new JLabel("Y:");
+        textFieldY = new JTextField("0", 10);
+        textFieldY.setMaximumSize(textFieldY.getPreferredSize());
+        JLabel labelZ = new JLabel("z:");
+        textFieldZ = new JTextField("0", 10);
+        textFieldZ.setMaximumSize(textFieldZ.getPreferredSize());
+        Box hboxVariables = Box.createHorizontalBox();
+        hboxVariables.add(Box.createHorizontalGlue());
+        hboxVariables.add(labelX);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldX);
+        hboxVariables.add(Box.createHorizontalStrut(50));
+        hboxVariables.add(labelY);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldY);
+        hboxVariables.add(Box.createHorizontalStrut(50));
+        hboxVariables.add(labelZ);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldZ);
+        hboxVariables.add(Box.createHorizontalGlue());
+        //Добавление области для вывода рузультата
+        JLabel labelResult = new JLabel("Результат:");
+        textFieldResult = new JTextField("0",15);
+        textFieldResult.setMaximumSize(textFieldResult.getPreferredSize());
+        Box hboxResult = Box.createHorizontalBox();
+        hboxResult.add(Box.createHorizontalGlue());
+        hboxResult.add(labelResult);
+        hboxResult.add(Box.createHorizontalStrut(10));
+        hboxResult.add(textFieldResult);
+        hboxResult.add(Box.createHorizontalGlue());
+        textFieldResult.setEditable(false);
+        //создвние кнопок
+        JButton buttonVichislit = new JButton("Вычислить");
+        buttonVichislit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                try{
+                    Double x = Double.parseDouble(textFieldX.getText());
+                    Double y = Double.parseDouble(textFieldY.getText());
+                    Double z = Double.parseDouble(textFieldZ.getText());
+                    Double result;
+                    if (formulaId == 1)
+                        result = Formula_1(x, y, z);
+                    else
+                        result = Formula_2(x, y, z);
+                    textFieldResult.setText(result.toString());
+                } catch (NumberFormatException ex){
+                    JOptionPane.showMessageDialog(Formula.this,"Ошибка в формате записи числа с " +
+                            "плавающей точкой", "Ошибочный формат числа", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+        JButton buttonErase = new JButton("Очистить поля");
+        buttonErase.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                textFieldX.setText("0");
+                textFieldY.setText("0");
+                textFieldZ.setText("0");
+                textFieldResult.setText("0");
+            }
+        });
+        Box hboxButtons = Box.createHorizontalBox();
+        hboxButtons.add(Box.createHorizontalGlue());
+        hboxButtons.add(buttonVichislit);
+        hboxButtons.add(Box.createHorizontalStrut(40));
+        hboxButtons.add(buttonErase);
+        hboxButtons.add(Box.createHorizontalGlue());
+        //Сборка панелей окна
+        Box vboxOkna = Box.createVerticalBox();
+        vboxOkna.add(Box.createVerticalGlue());
+        vboxOkna.add(hboxFormulaType);
+        vboxOkna.add(hboxVariables);
+        vboxOkna.add(Box.createVerticalGlue());
+        vboxOkna.add(hboxResult);
+        vboxOkna.add(Box.createVerticalGlue());
+        vboxOkna.add(hboxButtons);
+        vboxOkna.add(Box.createVerticalGlue());
+        getContentPane().add(vboxOkna, BorderLayout.CENTER);
+    }
+}
---
+++ src/bsu/rfe/java/group8/laba2/Guscha/varA9/Main.java
@@ -0,0 +1,15 @@
+package bsu.rfe.java.group8.laba2.Guscha.varA9;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //создаем экзкмпляр фрейма
+        Formula frame = new Formula();
+        //создаем реакцию на нажатие закрытия фрейма
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //показываем фрейм на экране
+        frame.setVisible(true);
+    }
+}
